e9b10e1e8ad637be68f6b6b4138e41b1a69f0817
bybhargav <64219094+bybhargav@users.noreply.github.com>
2026-08-01 00:42:50 +0530

    Implement Git object lookup and decompression

--- gitinfo.py
+++ gitinfo.py
@@ -1,20 +1,36 @@
 from pathlib import Path
-
-
+import zlib
 def read_head(path:Path):
     
     main_path = open(path/"HEAD", "r")
-    content = main_path.read()
+    content = main_path.read().strip()
     main_path.close()
     return content.removeprefix("ref: ")
 
 def resolve_head(path:Path):
     return path/read_head(path)
 
-def main():
+def read_ref(path: Path):
+    file = open(path, "r")
+    content = file.read().strip()
+    file.close()
+    return content 
 
-    repo = Path("/Users/sairaju/Documents/bybhargav/GitRank/.git")
-    print(read_head(repo))
+def locate_object(path: Path, commit_hash: str):
+    return path / "objects" / commit_hash[:2] / commit_hash[2:]
+
+def read_object(object_path: Path) -> bytes:
+    file = open(object_path,"rb")
+    byte_data = file.read()
+    file.close()
+    return byte_data
+
+def decompress_object(byte_data: bytes) -> bytes:
+    return zlib.decompress(byte_data)
+
+
+def main():
+    repo = Path(".git")
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 from validator import path_validator
-from gitinfo import resolve_head
+from gitinfo import resolve_head, read_ref, locate_object, read_object, decompress_object
 
 if len(sys.argv) == 1:
     sys.exit("Usage: python main.py <repository_path>")
@@ -14,3 +14,11 @@ if git_path is None:
     sys.exit("Error: This directory doesn't contain any .git folder")
 
 head_ref = resolve_head(git_path)
+current_commit = read_ref(head_ref)
+
+object_path = locate_object(git_path,current_commit)
+byte_data = read_object(object_path)
+decompressed_data = decompress_object(byte_data)
+
+print(type(decompressed_data))
+print(decompressed_data)
